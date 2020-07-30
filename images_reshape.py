53767a3ff07b7d53a20459da194050d65b58231a
harshayelleni <harshayelleni.gmail.com>
2020-07-30 13:24:29 +0530

    Aug4

--- images_reshape.py
+++ images_reshape.py
@@ -9,7 +9,7 @@ Created on Sat Jul  4 08:39:21 2020
 from PIL import Image
 import os, sys
 
-path = "/media/data/sanjay/person-segmentation/masks/" #"C:/Users/saiha/Downloads/OPPO/apppics/" #/root/Desktop/python/images/
+path = "/media/data/sanjay/person-segmentation/images/" #"C:/Users/saiha/Downloads/OPPO/apppics/" #/root/Desktop/python/images/
 dirs = os.listdir( path )
 
 def resize():
@@ -17,7 +17,7 @@ def resize():
         if os.path.isfile(path+item):
             im = Image.open(path+item)
             f, e = os.path.splitext(path+item)
-            imResize = im.resize((224,224), Image.ANTIALIAS)
+            imResize = im.resize((128,128), Image.ANTIALIAS)
             imResize.save(f + '_resized.png', 'PNG', quality=90) #JPEG
 
 resize()
